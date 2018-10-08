9449470fb471b51f39acc90a8a44138a9c923e51
rafaelatabug123 <2015081032@ust-ics.mygbiz.com>
2018-10-08 23:44:04 +0800

    Initial Commit

--- app/src/main/java/rafael/atabug/com/atabugrafaellab5/MainActivity.java
+++ app/src/main/java/rafael/atabug/com/atabugrafaellab5/MainActivity.java
@@ -1,7 +1,10 @@
 package rafael.atabug.com.atabugrafaellab5;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        Intent i = new Intent(this, MyServices.class);
+            startService(i);
+        }
+
+        public void process(View v){
+
+            Intent i=null, chooser=null;
+            if(v.getId()==R.id.GoToAct2){
+                i = new Intent(this,Activity2.class);
+                startActivity(i);
+            } else if(v.getId()==R.id.GoToMap1){
+                i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse("geo:14.6152856,120.98835860431774"));
+                chooser = Intent.createChooser(i, "Open Map with ?");
+                startActivity(chooser);
+
+            }
+        }
 }
